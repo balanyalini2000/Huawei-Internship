a4aa04d1c04bbf4368c8294950431edf3b83d6ac
balanyalini2000 <65641011+balanyalini2000@users.noreply.github.com>
2022-03-29 08:13:04 +0530

    Create MyFragment.java

---
+++ MusicPlayer & DatePickerFragment/DatePickerFragment/MyFragment.java
@@ -0,0 +1,18 @@
+package com.example.datepickerfragment;
+
+import android.os.Bundle;
+import android.app.DatePickerDialog;
+import androidx.fragment.app.DialogFragment;
+import android.app.Dialog;
+import java.util.Calendar;
+
+public class MyFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        return new DatePickerDialog(getActivity(), (DatePickerDialog.OnDateSetListener) getActivity(), year, month, day);
